60f2f819fcfec5b94303778c1d46e10810b5a9e5
Eduardo Navas <eduardonavas9699@gmail.com>
2024-05-06 15:53:31 -0400

    nuevos cambios efectuados en conexion dios mio

--- conexion.php
+++ conexion.php
@@ -5,8 +5,10 @@ $usuario = 'root';
 $pass = '';
 
 try{
+   
     $pdo = new PDO($link, $usuario, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"));
     // echo 'Conectado';
+    echo 'chachito feliz';
 }
 
 catch(PDOExeption $e){
