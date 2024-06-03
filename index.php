376ef10cea4655b464241da197de74e8279a510a
Eduardo-NR <eduardonavas9699@gmail.com>
2024-06-02 23:49:40 -0430

    modals asistencia p añadidos

--- index.php
+++ index.php
@@ -30,6 +30,11 @@ $mostrar = $consulta->fetchAll();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>SRAT</title>
 </head>
+<style>
+  .buton {background-color: #1B83AD; color: #FFFFFF;}
+  .buton:hover{background: #047e69; color: #FFFFFF;}
+  .modalh{background-color: #1B83AD; color: #FFFFFF;}
+</style>
 <body class="bg-dark-subtle">
 
 <div class="container-fluid">
@@ -69,12 +74,17 @@ $mostrar = $consulta->fetchAll();
               <td class="text-center"><?php echo $dato_p['fecha_rp']?></td>
               <td class="text-center"><?php echo $dato_p['fecha_cp']?></td>
               <td class="text-center">
-              <i class="bi bi-pencil-square btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#EditModal_asistencia_p"></i>
+        <!-- Button trigger Modal Edit-Asistencia Técnica Programada -->
+              <i class="bi bi-pencil-square btn btn-outline-primary shadow" data-bs-toggle="modal" data-bs-target="#EditModal_asistencia_p"></i>
               <i class="bi bi-file-earmark-x btn btn-outline-danger shadow"></i>
               </td>
             </tr>
           </tbody>
         </table>
+        <!-- Button trigger Modal-Asistencia Programada ADD -->
+        <button type="button" class="buton btn w-50 mx-auto" data-bs-toggle="modal" data-bs-target="#Modal_asistencia_p">
+          Nueva Asistencia
+        </button>
       </div>
 
       <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
@@ -141,6 +151,127 @@ $mostrar = $consulta->fetchAll();
       </div>
     </div>
 <div class="row mt-4">
+
+<!-- Modal-Asistencia Programada ADD -->
+<div class="modal fade" id="Modal_asistencia_p" data-bs-backdrop="static" tabindex="-1" aria-labelledby="Modal_Label_p" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable modal-md">
+      <div class="modal-content">
+        <div class="modal-header modalh">
+          <h1 class="modal-title fs-5 mx-auto" id="Modal_Label_p">Asistencia Técnica Programada</h1>
+        </div>
+        <div class="modal-body">
+      <!-- Formulario para envio de informacion - Asistencia Programada -->
+          <form class="row g-3">
+            <div class="col-md-3 text-center">
+              <label for="validationNro_planilla" class="form-label fw-semibold">Nro.Planilla</label>
+              <input type="text" class="form-control" placeholder="ejmp: S015" id="validationNro_planilla" value="" required>
+            </div>
+            <div class="col-md-6 text-center">
+              <label for="validationDepartamento" class="form-label fw-semibold">Departamento</label>
+              <select class="form-select" id="validationDepartamento" required>
+                <option class="fw-light fst-italic" selected disabled value="">Seleccionar Departamento</option>
+                <option>U.A.I</option>
+                <option>O.A.C</option>
+                <option>R.R.H.H</option>
+                <option>D.C.A.D</option>
+                <option>D.C.A.C.O.P</option>
+                <option>Dirección Técnica</option>
+                <option>Dirección General</option>
+                <option>Servicios Jurídicos</option>
+                <option>Despacho Contralor</option>
+                <option>Dirección de Administración</option>
+                <option>Comunicación y Relaciones Públicas</option>
+                <option>Determinación de Responsabilidades</option>
+              </select>
+            </div>
+            <div class="col-md-3 text-center">
+              <label for="validationCtd_equipos" class="form-label fw-semibold">Ctd.Equipos</label>
+              <input type="text" class="form-control" placeholder="ejmp: 03" id="validationCtd_equipos" value="" required>
+            </div>
+            <div class="col-md-3 mt-4 text-center">
+              <label for="validationFecha_r" class="form-label fw-semibold">Fch.Recibido</label>
+              <input type="text" class="form-control" placeholder="dd/mm/aa" id="validationFecha_r" value="" required>
+            </div>
+            <div class="col-md-3 mt-4 text-center">
+              <label for="validationfecha_c" class="form-label fw-semibold">Fch.Corrección</label>
+              <input type="text" class="form-control" placeholder="dd/mm/aa" id="validationfecha_c" value="" required>
+            </div>
+            <div class="col-md-6 mt-4 text-center">
+              <label for="validationAct_ejec" class="form-label fw-semibold">Act.Ejecutadas</label>
+              <textarea type="text" class="form-control" placeholder="Describa las actividades ejecutadas..." id="validationAct_ejec" rows="3" required></textarea>
+            </div>
+            <div class="col-md-12 mt-4 mx-auto w-75">
+              <button class="buton btn w-100 mx-auto" style="  border: 0;" type="submit">Agregar</button>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Cerrar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!-- Modal Edit-Asistencia Programada -->
+<div class="modal fade" id="EditModal_asistencia_p" data-bs-backdrop="static" tabindex="-1" aria-labelledby="EditModal_Label_p" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header modalh">
+          <h1 class="modal-title fs-5 mx-auto" id="EditModal_Label_p">Asistencia Técnica Programda</h1>
+        </div>
+        <div class="modal-body">
+      <!-- Formulario Edit Modal Asistencia Técnica Proramada -->
+          <form class="row g-3">
+            <div class="col-md-3 text-center">
+              <label for="validationEdit_Nro_planilla" class="form-label fw-semibold">Nro.Planilla</label>
+              <input type="text" class="form-control" placeholder="ejmp: S015" id="validationEdit_Nro_planilla" value="" required>
+            </div>
+            <div class="col-md-6 text-center">
+              <label for="validationEdit_Departamento" class="form-label fw-semibold">Departamento</label>
+              <select class="form-select" id="validationEdit_Departamento" required>
+                <option class="fw-light fst-italic" selected disabled value="">Seleccionar Departamento</option>
+                <option>U.A.I</option>
+                <option>O.A.C</option>
+                <option>R.R.H.H</option>
+                <option>D.C.A.D</option>
+                <option>D.C.A.C.O.P</option>
+                <option>Dirección Técnica</option>
+                <option>Dirección General</option>
+                <option>Servicios Jurídicos</option>
+                <option>Despacho Contralor</option>
+                <option>Dirección de Administración</option>
+                <option>Comunicación y Relaciones Públicas</option>
+                <option>Determinación de Responsabilidades</option>
+              </select>
+            </div>
+            <div class="col-md-3 text-center">
+              <label for="validationEdit_Ctd_equipos" class="form-label fw-semibold">Ctd.Equipos</label>
+              <input type="text" class="form-control" placeholder="ejmp: 03" id="validationEdit_Ctd_equipos" value="" required>
+            </div>
+            <div class="col-md-3 mt-4 text-center">
+              <label for="validationEdit_Fecha_r" class="form-label fw-semibold">Fch.Recibido</label>
+              <input type="text" class="form-control" placeholder="dd/mm/aa" id="validationEdit_Fecha_r" value="" required>
+            </div>
+            <div class="col-md-3 mt-4 text-center">
+              <label for="validationEdit_fecha_c" class="form-label fw-semibold">Fch.Corrección</label>
+              <input type="text" class="form-control" placeholder="dd/mm/aa" id="validationEdit_fecha_c" value="" required>
+            </div>
+            <div class="col-md-6 mt-4 text-center">
+              <label for="validationEdit_Act_ejec" class="form-label fw-semibold">Act.Ejecutadas</label>
+              <textarea type="text" class="form-control" placeholder="Describa las actividades ejecutadas..." id="validationEdit_Act_ejec" rows="3" required></textarea>
+            </div>
+            <div class="col-md-12 mt-4 mx-auto w-75">
+              <button class="buton btn w-100 mx-auto" style="  border: 0;" type="submit">Editar</button>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Cerrar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 <!-- carousel -->
 <div class="col-7">   
     <div id="carouselExampleInterval" class="carousel slide " data-bs-ride="carousel">
@@ -207,8 +338,13 @@ $mostrar = $consulta->fetchAll();
 </div>    
 </div>
 </div>
-</div>   
+</div>  
 
-<script src="js/bootstrap.bundle.js"></script>
+
+
+
+<!-- Script js -->
+  <script src="js/bootstrap.bundle.js"></script>
+  
 </body>
 </html>
